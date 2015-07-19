23a6091ac21bd1b331ed2d792a681909502257e5
Dennis A. Seidel <mail@dennisseidel.de>
2015-07-19 16:51:39 +0200

    Add POJO main method to make app exexutable

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by denbox on 19/07/15.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+        SpringBootApplication.run(Application.class, args);
+    }
+
+}

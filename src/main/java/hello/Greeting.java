fb5bd8b5c4ce4d48776502bb10597f2d52041a1a
Dennis A. Seidel <mail@dennisseidel.de>
2015-07-19 16:23:49 +0200

    Add resource representation class for greeting object

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,22 @@
+package hello;
+
+/**
+ * Created by denbox on 19/07/15.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public String getContent(){
+        return content;
+    }
+}
